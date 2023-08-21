3badf90d531ef35bae62cfdb39885c6feb181808
Ashutosh Patil <patilashutosh67@gmail.com>
2023-08-21 20:03:53 +0530

    Wrote getComputerChoice function to get random value from rock, paper, scissors

--- game.js
+++ game.js
@@ -1,1 +1,7 @@
 console.log('Working..')
+
+function getComputerChoice(){
+    const choices=['Rock','Paper','Scissors'];
+    const randomIndex= Math.floor(Math.random()*choices.length);
+    return choices[randomIndex];
+}
