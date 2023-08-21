e3713bca3df13e120aa1cc0ab18bb17e78e50430
Ashutosh Patil <patilashutosh67@gmail.com>
2023-08-21 20:49:13 +0530

    Created 1 round game of rock-paper-scissor with output being displayed on console

--- game.js
+++ game.js
@@ -1,7 +1,26 @@
 console.log('Working..')
-
+// computer choice
 function getComputerChoice(){
     const choices=['Rock','Paper','Scissors'];
     const randomIndex= Math.floor(Math.random()*choices.length);
     return choices[randomIndex];
 }
+
+// 1 round of game
+
+function playRound(playerSelection,computerSelection){
+    playerSelection= playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    if(playerSelection===computerSelection){
+        return "Tie"
+    }
+    else if((playerSelection==='rock' && computerSelection==='scissors') || 
+    (playerSelection==='paper' && computerSelection==='rock') || 
+    (playerSelection==='scissors' && computerSelection==='paper')){
+        return `You win ${playerSelection} beats ${computerSelection}`
+    }
+    else{
+        return `You lose ${computerSelection} beats ${playerSelection}`
+     }
+}
+
